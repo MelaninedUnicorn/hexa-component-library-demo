e7cb2f54a45b3825f26f02a4e5dbb989d37252f8
Mugisha Joyce-Christiane Kakou <mugisha.kakou@gmail.com>
2022-01-11 17:00:16 +0100

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,27 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-const FormComponent: React.FC<any> = () => (
-    <div>
-        <Box
-            data-testid="form-component"
-            className={`form-component-container`}
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <Typography>Color Code Visualizer</Typography>
-            <TextField id="color-code-input" label="Color Code" variant="outlined" />
-            <Button type="submit" id="submitBtn" variant="contained"> Generate Color</Button>
-            <Button type="submit" id="submitBtn" color="success" variant="contained"> Submit</Button>
+const FormComponent: React.FC<any> = () => {
+    const [colorCode, setcolorCode] = useState("#4666d0");
+    const submitForm = (event: any) => {
 
-        </Box>
-    </div>
-);
+        // verify that the value is a hex value
+        // call the fct from my npm package
+        // if the value is hex value update the state
+    }
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setcolorCode(event.target.value);
+    };
+    return (
+        <div>
+            <Box
+                data-testid="form-component"
+                className={`form-component-container`}
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <Typography>Color Code Visualizer</Typography>
+                <TextField id="color-code-input" label="Color Code" variant="outlined" value={colorCode} onChange={handleChange} />
+                <Button id="submitBtn" variant="contained"> Generate Color</Button>
+                <Button type="submit" id="submitBtn" color="success" variant="contained" onClick={submitForm} > Submit</Button>
+
+            </Box>
+        </div>
+    )
+};
 
 export default FormComponent;
